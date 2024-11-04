71fc3958f6e1326beeee04009e70ac2665062036
Marwane Bidamou <devr.marwane@gmail.com>
2024-11-03 21:38:16 -0600

    Time: 3 ms (5.05%) | Memory: 38.8 MB (41.26%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cs
@@ -0,0 +1,31 @@
+public class Solution
+{
+    public IList<IList<int>> Generate(int numRows)
+    {
+        IList<IList<int>> list = new List<IList<int>>();
+
+        list.Add(new List<int>() { 1 });
+
+        if (numRows == 1)
+            return list;
+
+        for (int i = 1; i < numRows; i++)
+        {
+            list.Add(Enumerable.Repeat(0, i+1).ToList());
+            int columnIndex = 0;
+            list[i][columnIndex++] = list[i - 1][0];
+
+            int j = 0;
+            while (j < list[i - 1].Count - 1)
+            {
+                list[i][columnIndex++] = list[i - 1][j] + (j >= list[i - 1].Count ? 0 : list[i - 1][j + 1]);
+                j++;
+            }
+            list[i][columnIndex++] = list[i - 1][^1];
+
+            columnIndex = 0;
+        }
+
+        return list;
+    }
+}
