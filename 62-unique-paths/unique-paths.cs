4aadf07c2e7cbcc8f407249d2da403f722fdca69
Marwane Bidamou <devr.marwane@gmail.com>
2024-11-22 16:25:59 -0600

    Time: 1 ms (27.25%) | Memory: 27.9 MB (11.69%) - LeetSync

--- 62-unique-paths/unique-paths.cs
+++ 62-unique-paths/unique-paths.cs
@@ -8,38 +8,20 @@ public class Solution {
 
     public int UniquePathsHelper(int m, int n, Dictionary<string,int> memo) {
 
+        var memoKey = m+","+n;
+
+        if (memo.ContainsKey(memoKey)){
+            return memo[memoKey];
+        }
+
         if (m== 0 || n==0) //empty grid
             return 0;
 
         if (m == 1 && n == 1) //1x1 grid: there is only way which is we do nothing
             return 1;
         
-
-        int topCellPaths;
-        var memoTopKey = Math.Min(m-1, n)+"-"+Math.Max(m-1, n);
-
-        if (memo.ContainsKey(memoTopKey)){
-            Console.WriteLine(memoTopKey);
-            topCellPaths=memo[memoTopKey];
-        }
-        else{
-            topCellPaths=UniquePathsHelper(m-1, n, memo);
-            memo.Add(memoTopKey, topCellPaths); 
-        }
-
-        int leftCellPaths;
-        var memoLeftKey = Math.Min(m, n-1)+"-"+Math.Max(m, n-1);
+        memo[memoKey] = UniquePathsHelper(m-1, n, memo) +  UniquePathsHelper(m, n-1, memo);
         
-        if (memo.ContainsKey(memoLeftKey)){
-            Console.WriteLine(memoLeftKey);
-            leftCellPaths=memo[memoLeftKey];
-        }
-        else{
-            leftCellPaths=UniquePathsHelper(m, n-1, memo);
-            memo.Add(memoLeftKey, leftCellPaths); 
-        }
-        
-        return  leftCellPaths + topCellPaths; 
+        return memo[memoKey] ; 
     }
-
 }
