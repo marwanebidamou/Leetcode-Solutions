e776b3ba8d3411b0986340cfeaf681877cc779d6
Marwane Bidamou <devr.marwane@gmail.com>
2024-11-29 21:18:50 -0600

    Time: 1 ms (100.00%) | Memory: 63.5 MB (73.81%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.cs
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.cs
@@ -1,5 +1,7 @@
 public class Solution {
     public int WaysToSplitArray(int[] nums) {
+        /*
+        // Solving the problem using PrefixSum array
         if(nums.Length<2)
             return 0;
 
@@ -17,6 +19,31 @@ public class Solution {
         {
             long firstPart = sumArray[i];
             long lastPart = sumArray[nums.Length-1] - sumArray[i];
+            
+            if( firstPart >= lastPart )
+                validSplitsCount++;
+        }
+
+        return validSplitsCount;
+
+        */
+
+        // Solving the problem without the additonal array
+        if(nums.Length<2)
+            return 0;
+
+        long sum=0;
+        for(int i=0;i<nums.Length;i++)
+        {
+            sum = sum + nums[i];
+        }
+
+        int validSplitsCount = 0;
+        long firstPart = 0;
+        for(int i=0;i<nums.Length-1;i++)
+        {
+            firstPart += nums[i];
+            long lastPart = sum - firstPart;
             
             if( firstPart >= lastPart )
                 validSplitsCount++;
