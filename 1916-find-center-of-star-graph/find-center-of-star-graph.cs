1262df9d274da52ddcc566e760fef0fe35e9c00b
Marwane Bidamou <devr.marwane@gmail.com>
2024-11-30 20:11:16 -0600

    Time: 266 ms (5.17%) | Memory: 76.2 MB (25.90%) - LeetSync

---
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int FindCenter(int[][] edges) {
+        
+        int[] vertices = new int[edges.Length+2];
+
+        for(int i=0;i<edges.Length;i++)
+        {
+             Console.WriteLine(edges[i][0]+" - "+ edges[i][1]);
+            
+            vertices[edges[i][0]]++;
+            vertices[edges[i][1]]++;
+
+            if(vertices[edges[i][0]]==edges.Length)
+            {
+                return edges[i][0];
+            }
+
+            if(vertices[edges[i][1]]==edges.Length)
+            {
+                return edges[i][1];
+            }
+        }
+        return 0;
+    }
+}
