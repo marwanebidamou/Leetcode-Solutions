b3518131553db91d128dafb7d26c6223db5b7063
Marwane Bidamou <devr.marwane@gmail.com>
2024-12-19 15:15:26 -0600

    Time: 93 ms (71.24%) | Memory: 47.1 MB (43.38%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cs
+++ 141-linked-list-cycle/linked-list-cycle.cs
@@ -11,6 +11,10 @@
  */
 public class Solution {
     public bool HasCycle(ListNode head) {
+
+        /* 
+        //Solving the problem using HashSet
+
         HashSet<ListNode> map = new HashSet<ListNode>();
 
         while(head!=null){
@@ -18,6 +22,27 @@ public class Solution {
                 return true;
             head = head.next;
         }
+        return false;
+
+        */
+
+        //Solving the problem using slow / fast pointer
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast?.next != null)
+        {
+            fast = fast.next.next;
+            slow = slow.next;
+
+
+            // If the 'fast' pointer meets the 'slow' pointer, it means there is a cycle.
+            // This is because both pointers are now inside the cycle, and since 'fast' moves
+            // twice as fast as 'slow', it will eventually "lap" the slower pointer.
+            if(fast == slow)
+                return true;
+        }
+
         return false;
     }
 }
