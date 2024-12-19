8337f86d62b9b6ded89f9fd28fd54cbab8920785
Marwane Bidamou <devr.marwane@gmail.com>
2024-12-19 14:51:44 -0600

    Time: 105 ms (23.81%) | Memory: 48.4 MB (13.83%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.cs
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool HasCycle(ListNode head) {
+        HashSet<ListNode> map = new HashSet<ListNode>();
+
+        while(head!=null){
+            if(!map.Add(head))
+                return true;
+            head = head.next;
+        }
+        return false;
+    }
+}
