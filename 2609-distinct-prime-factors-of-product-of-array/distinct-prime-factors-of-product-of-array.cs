4e8b3c8bbe6b7722ee6bbf969180611f96f06fcc
Marwane Bidamou <devr.marwane@gmail.com>
2024-12-28 21:27:48 -0600

    Time: 70 ms (20.00%) | Memory: 58.4 MB (10.00%) - LeetSync

--- 2609-distinct-prime-factors-of-product-of-array/distinct-prime-factors-of-product-of-array.cs
+++ 2609-distinct-prime-factors-of-product-of-array/distinct-prime-factors-of-product-of-array.cs
@@ -1,38 +1,43 @@
-public class Solution {
-    public int DistinctPrimeFactors(int[] nums) {
-
+public class Solution
+{
+    public int DistinctPrimeFactors(int[] nums)
+    {
         HashSet<int> hash = new HashSet<int>();
 
-        for(int i=0;i<nums.Length;i++)
+        foreach (int num in nums)
         {
-            hash.UnionWith(GetPrimeFactors(nums[i]));
-
+            hash.UnionWith(GetPrimeFactors(num));
         }
-        return hash.Count();
+        
+        return hash.Count;
     }
 
-
-    HashSet<int> GetPrimeFactors(int number)
+    private HashSet<int> GetPrimeFactors(int number)
     {
         HashSet<int> factors = new HashSet<int>();
 
-        while(number%2==0)
+        // Factor out 2 to handle even numbers.
+        while (number % 2 == 0)
         {
-            number = number / 2;
             factors.Add(2);
+            number /= 2;
         }
 
-        for(int i= 3;i<number;i=i+2)
+        // Check odd numbers up to the square root of 'number'.
+        for (int i = 3; i * i <= number; i += 2)
         {
-            while(number%i==0)
+            while (number % i == 0)
             {
-                number = number / i;
                 factors.Add(i);
+                number /= i;
             }
         }
 
-        if(number>2)
+        // If 'number' is still greater than 2, it is prime.
+        if (number > 2)
+        {
             factors.Add(number);
+        }
 
         return factors;
     }
