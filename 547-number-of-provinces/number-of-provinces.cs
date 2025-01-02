c8caf7d6028a18e9d9fa55ce5a93b92128e63270
Marwane Bidamou <devr.marwane@gmail.com>
2025-01-02 13:27:26 -0600

    Time: 2 ms (94.88%) | Memory: 49.3 MB (58.09%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cs
+++ 547-number-of-provinces/number-of-provinces.cs
@@ -1,60 +1,29 @@
 public class Solution {
     public int FindCircleNum(int[][] isConnected) {
-        
         int n = isConnected.Length;
-        int[] citiesProvinces = new int[n];
-        int visitedCount = 0;
+        bool[] visited = new bool[n]; // Tracks visited cities
+        int provinces = 0;
 
-        //Convert matrix to adjacent list
-        Dictionary<int, List<int>> graph = new Dictionary<int, List<int>>();
-
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(isConnected[i][j]==1)
-                {
-                    if(graph.ContainsKey(i))
-                        graph[i].Add(j);
-                    else
-                        graph.Add(i, new List<int>{j});
+        // Helper function to perform DFS
+        void DFS(int city) {
+            for (int neighbor = 0; neighbor < n; neighbor++) {
+                // If there's a connection and the neighbor hasn't been visited
+                if (isConnected[city][neighbor] == 1 && !visited[neighbor]) {
+                    visited[neighbor] = true;
+                    DFS(neighbor); // Visit the neighbor
                 }
             }
         }
 
-        int province = 1;
-        while(visitedCount < n)
-        {
-            Stack<int> stack = new Stack<int>();
-            //get next not visited city
-            int city=0;
-            while(citiesProvinces[city]!=0)
-                city++;
-            
-            stack.Push(city);
-            citiesProvinces[city] = province;
-            visitedCount++;
-
-            while(stack.Any())
-            {
-                var currCity = stack.Pop();
-                
-                //get neighbors
-                var neighbors = graph[currCity];
-                foreach(var neighbor in neighbors)
-                {
-                    if(citiesProvinces[neighbor]==0)
-                    {
-                        citiesProvinces[neighbor] = province;
-                        stack.Push(neighbor);
-                        visitedCount++;
-                    }
-                }
+        // Iterate through all cities
+        for (int city = 0; city < n; city++) {
+            if (!visited[city]) {
+                provinces++;
+                visited[city] = true;
+                DFS(city); // Start a DFS from the unvisited city
             }
-
-            province++;
         }
 
-        return province-1;
+        return provinces;
     }
 }
