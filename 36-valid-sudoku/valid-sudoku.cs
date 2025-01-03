1461a106b638353a4443cddcfbf75b51ab9fc498
Marwane Bidamou <devr.marwane@gmail.com>
2025-01-02 21:07:56 -0600

    Time: 7 ms (17.43%) | Memory: 49 MB (28.59%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cs
+++ 36-valid-sudoku/valid-sudoku.cs
@@ -1,46 +1,43 @@
-public class Solution {
-    public bool IsValidSudoku(char[][] board) {
-        
-        int n=board.Length;
+public class Solution
+{
+    public bool IsValidSudoku(char[][] board)
+    {
+        int n = board.Length;
+        // Arrays of HashSet to track numbers seen in each row, column, and 3x3 box
+        HashSet<char>[] rows = new HashSet<char>[n];
+        HashSet<char>[] cols = new HashSet<char>[n];
+        HashSet<char>[] boxes = new HashSet<char>[n];
 
-
-        for(int i=0;i<n;i++)
+        // Initialize HashSet for each row, column, and box
+        for (int i = 0; i < n; i++)
         {
-            HashSet<char> row = new HashSet<char>();
-            HashSet<char> col = new HashSet<char>();
+            rows[i] = new HashSet<char>();
+            cols[i] = new HashSet<char>();
+            boxes[i] = new HashSet<char>();
+        }
 
-            for(int j=0;j<n;j++)
+        // Iterate through every cell in the board
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
             {
-                Console.WriteLine(" row "+board[i][j]+" col "+board[j][i]);
-                if(board[i][j]!= '.' && !row.Add(board[i][j]))
-                    return false;
-                
-                if(board[j][i]!= '.' && !col.Add(board[j][i]))
-                    return false;
+                char num = board[i][j];
+
+                if (num == '.') continue;
+
+                // Validate row
+                if (!rows[i].Add(num)) return false;
+
+                // Validate column
+                if (!cols[j].Add(num)) return false;
+
+                // Validate 3x3 box
+                // Calculate the index of the box that the current cell (i, j) belongs to
+                int boxIndex = (i / 3) * 3 + j / 3;
+                if (!boxes[boxIndex].Add(num)) return false;
             }
         }
 
-        int numberOfBoxPerLine = n/3;
-
-
-        for(int i=0;i<n;i+=3)
-        {
-            for(int j=0;j<n;j+=3)
-            {
-                HashSet<char> vals = new HashSet<char>();
-                for(int row=0;row<3;row++)
-                {
-                    for(int col=0;col<3;col++)
-                    {
-                        var cell = board[i+row][j+col];
-                        if(cell!='.' && !vals.Add(cell))
-                            return false;
-                    }
-                }                
-            }
-        }
-        
         return true;
-
-    }   
+    }
 }
