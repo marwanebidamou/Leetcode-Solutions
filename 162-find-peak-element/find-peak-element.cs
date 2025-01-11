6e52cdf65a1b3539c86ae64cfbd7ae3e922a4c57
Marwane Bidamou <devr.marwane@gmail.com>
2025-01-10 22:32:04 -0600

    Time: 23 ms (1.74%) | Memory: 42.9 MB (5.86%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int FindPeakElement(int[] nums) {
+
+        return Helper(nums, 0, nums.Length-1);
+
+        
+    }
+
+    public int Helper(int[] nums, int left, int right)
+    {
+        if(left>right)
+            return -1;
+
+        int mid = (left+right)/2;
+        Console.WriteLine(mid);
+        if((mid==0 || nums[mid-1]<nums[mid]) && (mid==nums.Length-1 || nums[mid+1]<nums[mid]))
+            return mid;
+        
+        int leftSide = Helper(nums, left, mid-1);
+        if(leftSide != -1)
+            return leftSide;
+        
+        return Helper(nums, mid+1, right);
+
+        
+    }
+}
