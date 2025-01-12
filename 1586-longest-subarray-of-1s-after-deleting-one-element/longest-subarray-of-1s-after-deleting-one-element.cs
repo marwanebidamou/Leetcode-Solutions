0f0dc0d3e5330de051cfa8a528ac5f12e67b686d
Marwane Bidamou <devr.marwane@gmail.com>
2025-01-11 21:10:49 -0600

    Time: 1 ms (100.00%) | Memory: 55.1 MB (9.15%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cs
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cs
@@ -1,35 +1,29 @@
 public class Solution {
     public int LongestSubarray(int[] nums) {
-        
-        int lastestZeroIndex = -1;
-        int prevZeroIndex = -1;
+        int lastestZeroIndex = -1; // Tracks the most recent 0 index
+        int prevZeroIndex = -1;    // Tracks the second most recent 0 index
+        int longestSubArray = 0;   // Stores the maximum length of the subarray of 1s
 
-        int longestSubArray = 0;
-
-        for(int i=0;i<nums.Length;i++)
-        {
-            if(nums[i]==0)
-            {
-                if(lastestZeroIndex == -1)
-                    lastestZeroIndex = i;
-                else
-                {
-                    prevZeroIndex = lastestZeroIndex;
-                    lastestZeroIndex = i;
-                }             
+        for (int i = 0; i < nums.Length; i++) {
+            if (nums[i] == 0) {
+                // Update the indices of the most recent and second most recent 0s
+                prevZeroIndex = lastestZeroIndex;
+                lastestZeroIndex = i;
             }
-            else
-            {
-                if(prevZeroIndex == -1 && lastestZeroIndex==-1)
-                    longestSubArray = Math.Max(longestSubArray, i+1);
-                else
-                    longestSubArray = Math.Max(longestSubArray, i-prevZeroIndex-1);
+            // Calculate the current subarray length of 1s, skipping at most one 0
+            int currentLength;
+            if (lastestZeroIndex == -1 ) {
+                currentLength = i + 1; // No second 0 encountered yet
+            } else {
+                currentLength = i - prevZeroIndex - 1; // Skip the earlier 0
             }
-
+            longestSubArray = Math.Max(longestSubArray, currentLength);
         }
 
-        if(lastestZeroIndex==-1)
-            return nums.Length-1;
+        // Edge case: If there is no 0 in the array, return nums.Length - 1
+        if (lastestZeroIndex == -1) {
+            return nums.Length - 1;
+        }
 
         return longestSubArray;
     }
