7d2d008d9c0a2378a246a921bad39e9639bf9a42
Marwane Bidamou <devr.marwane@gmail.com>
2025-01-11 21:12:36 -0600

    Time: 1 ms (100.00%) | Memory: 55 MB (16.12%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cs
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cs
@@ -1,9 +1,8 @@
 public class Solution {
     public int LongestSubarray(int[] nums) {
-        int lastestZeroIndex = -1; // Tracks the most recent 0 index
-        int prevZeroIndex = -1;    // Tracks the second most recent 0 index
-        int longestSubArray = 0;   // Stores the maximum length of the subarray of 1s
-
+        int lastestZeroIndex = -1; // 1st most recent 0 index
+        int prevZeroIndex = -1;    // 2nd most recent 0 index
+        int longestSubArray = 0;  
         for (int i = 0; i < nums.Length; i++) {
             if (nums[i] == 0) {
                 // Update the indices of the most recent and second most recent 0s
@@ -13,14 +12,14 @@ public class Solution {
             // Calculate the current subarray length of 1s, skipping at most one 0
             int currentLength;
             if (lastestZeroIndex == -1 ) {
-                currentLength = i + 1; // No second 0 encountered yet
+                currentLength = i + 1; // No 0 encountered yet
             } else {
                 currentLength = i - prevZeroIndex - 1; // Skip the earlier 0
             }
             longestSubArray = Math.Max(longestSubArray, currentLength);
         }
 
-        // Edge case: If there is no 0 in the array, return nums.Length - 1
+        // no 0 in the array
         if (lastestZeroIndex == -1) {
             return nums.Length - 1;
         }
