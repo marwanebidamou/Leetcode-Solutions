b7bb9f2d5f096f8baa2ee603119162367e36c8b0
Marwane Bidamou <devr.marwane@gmail.com>
2025-01-15 22:54:52 -0600

    Time: 9 ms (15.58%) | Memory: 50.5 MB (11.04%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cs
+++ 735-asteroid-collision/asteroid-collision.cs
@@ -1,28 +1,36 @@
 public class Solution {
     public int[] AsteroidCollision(int[] asteroids) {
+        // Stack to keep track of surviving asteroids
         Stack<int> stack = new Stack<int>();
 
         foreach (var asteroid in asteroids) {
-            bool isDestroyed = false;
+            bool isDestroyed = false; // Flag to check if the current asteroid is destroyed
 
+            // Check for collisions: stack has a positive asteroid and the current one is negative
             while (stack.Any() && asteroid < 0 && stack.Peek() > 0) {
+                // Compare absolute values of the colliding asteroids
                 if (Math.Abs(asteroid) > Math.Abs(stack.Peek())) {
-                    stack.Pop(); 
+                    // Current asteroid destroys the top of the stack
+                    stack.Pop();
                 } else if (Math.Abs(asteroid) == Math.Abs(stack.Peek())) {
-                    stack.Pop(); 
-                    isDestroyed = true;
+                    // Both asteroids are destroyed
+                    stack.Pop();
+                    isDestroyed = true; // Mark the current asteroid as destroyed
                     break;
                 } else {
+                    // Top of the stack survives, current asteroid is destroyed
                     isDestroyed = true;
                     break;
                 }
             }
 
+            // If the current asteroid is not destroyed, push it onto the stack
             if (!isDestroyed) {
                 stack.Push(asteroid);
             }
         }
 
+        // Return the remaining asteroids in the correct order
         return stack.Reverse().ToArray();
     }
 }
