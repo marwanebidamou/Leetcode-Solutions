7140bbf3d3015fc5d684659e68fb42b2fba3acfa
Marwane Bidamou <devr.marwane@gmail.com>
2025-01-16 23:18:34 -0600

    Time: 3 ms (96.12%) | Memory: 43.9 MB (88.79%) - LeetSync

--- 179-largest-number/largest-number.cs
+++ 179-largest-number/largest-number.cs
@@ -6,6 +6,9 @@ public class Solution
         string[] numStrings = nums.Select(num => num.ToString()).ToArray();
 
         // Sort strings based on concatenated values
+        // Sort the strings based on concatenated values (b + a vs. a + b) to ensure
+        // that the resulting concatenation forms the largest possible number.
+        // This comparison guarantees that the larger concatenation order comes first in the sorted array.
         Array.Sort(numStrings, (a, b) => string.Compare(b + a, a + b, StringComparison.Ordinal));
 
         // Handle the case where the largest number is zero
