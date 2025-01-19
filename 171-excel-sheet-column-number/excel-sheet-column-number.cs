df39f892c495bc9328f5f45fc46c772b529b53bd
Marwane Bidamou <devr.marwane@gmail.com>
2025-01-18 22:21:13 -0600

    Time: 0 ms (100.00%) | Memory: 40.7 MB (31.08%) - LeetSync

--- 171-excel-sheet-column-number/excel-sheet-column-number.cs
+++ 171-excel-sheet-column-number/excel-sheet-column-number.cs
@@ -3,8 +3,8 @@ public class Solution {
         int result = 0;
 
         foreach (char c in columnTitle) {
-            int value = c - 'A' + 1;
-            result = result * 26 + value;
+            int value = c - 'A' + 1; // Map 'A' to 1, 'B' to 2, ..., 'Z' to 26
+            result = result * 26 + value; // Update result for each character
         }
 
         return result;
