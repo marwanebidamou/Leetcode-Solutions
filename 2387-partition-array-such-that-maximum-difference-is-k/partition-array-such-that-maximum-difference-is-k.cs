adc32f661e3b7386d34ab56716bc1a9aef4c51a1
Marwane Bidamou <devr.marwane@gmail.com>
2025-02-24 20:38:46 -0600

    Time: 121 ms (92.00%) | Memory: 75.9 MB (44.00%) - LeetSync

--- 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.cs
+++ 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.cs
@@ -1,16 +1,14 @@
 public class Solution {
     public int PartitionArray(int[] nums, int k) {
-        Array.Sort(nums);
+        Array.Sort(nums); // Step 1: Sort the array
+        
+        int partitionMin = nums[0]; // First element of the partition
+        int count = 1; // At least one partition needed
 
-        int x=nums[0];
-        int count = 1;
-
-        for(int i=1;i<nums.Length;i++)
-        {
-            if(nums[i]-x > k)
-            {
-                count++;
-                x = nums[i];
+        for (int i = 1; i < nums.Length; i++) {
+            if (nums[i] - partitionMin > k) {
+                count++; // Start a new partition
+                partitionMin = nums[i]; // Reset the minimum element of the new partition
             }
         }
 
