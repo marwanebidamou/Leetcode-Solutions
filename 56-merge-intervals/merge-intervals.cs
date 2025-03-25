fecfea0f2fe7e974b65ee02d29e52773575282b1
Marwane Bidamou <devr.marwane@gmail.com>
2025-03-25 15:09:57 -0500

    Time: 6 ms (38.38%) | Memory: 56.3 MB (68.14%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public int[][] Merge(int[][] intervals) {
+        
+        Array.Sort(intervals, (a,b)=> a[0]-b[0]);
+        
+        Stack<int[]> stack = new Stack<int[]>();
+
+        foreach(var item in intervals)
+        {
+            if(!stack.Any() || stack.Peek()[1] < item[0])
+            {
+                stack.Push(item);
+            }
+            else if(stack.Peek()[0] <= item[0] && stack.Peek()[1] >= item[1])
+            {
+                // Do nothing
+            }
+            else
+            {
+                var pop = stack.Pop();
+                stack.Push(new int[]{pop[0], item[1]});
+            }
+        }
+
+        return stack.ToArray();
+    }
+    
+}
