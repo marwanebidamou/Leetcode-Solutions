41b030ffecc1af76afe1d044ce10eb97ec0b8235
Marwane Bidamou <devr.marwane@gmail.com>
2025-03-26 13:00:25 -0500

    Time: 130 ms (7.84%) | Memory: 49.6 MB (36.27%) - LeetSync

--- 285-inorder-successor-in-bst/inorder-successor-in-bst.cs
+++ 285-inorder-successor-in-bst/inorder-successor-in-bst.cs
@@ -1,39 +1,16 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left;
- *     public TreeNode right;
- *     public TreeNode(int x) { val = x; }
- * }
- */
 public class Solution {
     public TreeNode InorderSuccessor(TreeNode root, TreeNode p) {
-        
-        Queue<TreeNode> queue = new Queue<TreeNode>();
-
-        queue.Enqueue(root);
-
         TreeNode successor = null;
-        while(queue.Any())
-        {
-            var curr = queue.Dequeue();
 
-            if(curr.val > p.val){
-                if(successor==null || successor.val > curr.val){
-                    successor = curr;
-                }
+        while (root != null) {
+            if (p.val < root.val) {
+                successor = root;
+                root = root.left;
+            } else {
+                root = root.right;
             }
-
-            if(curr.left!=null)
-                queue.Enqueue(curr.left);
-            
-            if(curr.right!=null)
-                queue.Enqueue(curr.right);
-            
         }
 
         return successor;
-
     }
 }
