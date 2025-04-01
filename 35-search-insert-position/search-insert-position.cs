c4ee502946b7d0e8f7e4e65aa376be13e3d3d987
Marwane Bidamou <devr.marwane@gmail.com>
2025-03-31 21:58:31 -0500

    Time: 0 ms (100.00%) | Memory: 43.1 MB (55.36%) - LeetSync

--- 35-search-insert-position/search-insert-position.cs
+++ 35-search-insert-position/search-insert-position.cs
@@ -1,9 +1,21 @@
 public class Solution {
     public int SearchInsert(int[] nums, int target) {
-        for(int i=0;i<nums.Length;i++){
-            if (nums[i] == target || nums[i]>target)
-                return i;
+    int left = 0;
+    int right = nums.Length - 1;
+        if (target <= nums[0]) return 0;
+        if (target > nums[right]) return nums.Length;
+
+        while(left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target)
+                return mid;
+            
+            left = nums[mid] < target ? mid + 1 : left;
+            right = nums[mid] > target ? mid - 1 : right;            
         }
-        return nums.Length;
+
+        return left;
     }
+
 }
